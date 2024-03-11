264284198cf00d5252c632bcfba24421b6bd0977
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-03-11 17:13:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from 'components/App/App';
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <App />
   </React.StrictMode>,
