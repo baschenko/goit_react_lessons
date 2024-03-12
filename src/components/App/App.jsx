8be5ee58f638b3361ca3bac7f59a3301a0342eb9
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-03-12 17:17:40 +0200

    add TodoList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,59 @@
 // import Counter from 'components/Counter';
-import ColorPicker from 'components/ColorPicker';
+// import ColorPicker from 'components/ColorPicker';
 // import Dropdown from 'components/Dropdown';
-import React from 'react';
+import TodoList from 'components/TodoList';
+import React, { Component } from 'react';
+import initialTodos from '../../todos.json';
 
-const colorPickerOption = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D86' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
+// const colorPickerOption = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D86' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
 
-];
+// ];
 
-const App = () => {
-  return (
-    <>
-      <h1>Состояние компонента</h1>
-      {/* <Counter /> */}
-      {/* <Dropdown/> */}
-      <ColorPicker options={colorPickerOption} />
-    </>
-  );
-};
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = (todoId) => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+
+
+  render() {
+    const { todos } = this.state;
+    
+    const coplitedTodosCount = todos.reduce((acc, todo) => (todo.complited ? acc+1: acc), 0);
+    return (
+      <>
+        <h1>Состояние компонента</h1>
+        {/* <Counter /> */}
+        {/* <Dropdown/> */}
+        {/* <ColorPicker options={colorPickerOption} /> */}
+        <div>
+          <p>
+            Общее кол-во Todo: {todos.length};
+          </p>
+          <p>
+            Кол-во выполненых: {coplitedTodosCount};
+          </p>
+        </div>
+        <TodoList
+          todos={todos}
+          onDeleteTodo={this.deleteTodo}
+        />
+      </>
+    );
+  }
+}
+
 
 export default App;
