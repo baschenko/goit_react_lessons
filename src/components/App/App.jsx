88b3d07b257ea9d3cfa3894dffeebbc2e3df7be3
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-03-12 14:39:23 +0200

    add ColorPicker

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,25 @@
-import Counter from 'components/Counter/Counter';
+// import Counter from 'components/Counter/Counter';
+import ColorPicker from 'components/ColorPicker';
+// import Dropdown from 'components/Dropdown/Dropdown';
 import React from 'react';
 
-// const colorPickerOption = [
-//   { label: 'red', color: '#F44336' },
-//   { label: 'green', color: '#4CAF50' },
-//   { label: 'blue', color: '#2196F3' },
-//   { label: 'grey', color: '#607D86' },
-//   { label: 'pink', color: '#E91E63' },
-//   { label: 'indigo', color: '#3F51B5' },
+const colorPickerOption = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D86' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
 
-// ];
+];
 
 const App = () => {
   return (
     <>
       <h1>Состояние компонента</h1>
       {/* <Counter /> */}
-      
+      {/* <Dropdown/> */}
+      <ColorPicker options={colorPickerOption} />
     </>
   );
 };
--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,17 +1,56 @@
-
+import { Component } from 'react';
+import React from 'react';
+import './Dropdown.css';
 
 class Dropdown extends Component {
-    render() {
-        return (
-            <div className="Dropdown">
-                <button type="button" className="Dropdown__toggle">
-                    Показать
-                </button>
-                <div className="Dropdown__menu">Выпадающее меню</div>
-            </div>
-            
-        );
-    }
+  state = {
+    visible: false,
+  };
+
+  toggle = () => {
+    this.setState(prevState => ({
+      visible: !prevState.visible,
+    }));
+  };
+
+  // show = () => {
+  //     this.setState({ visible: true });
+  // }
+
+  // hide = () => {
+  //     this.setState({ visible: false });
+  // }
+
+  render() {
+    const { visible } = this.state;
+    return (
+      <div className="Dropdown">
+        <button
+          type="button"
+          className="Dropdown__toggle"
+          // при нажатии
+          onClick={this.toggle}
+                
+           // при наведении
+           //onMouseOver={this.toggle} 
+        >
+          {!visible ? 'Показать' : 'Скрыть'}
+        </button>
+
+        {/* <button
+              type="button"
+              className="Dropdown__toggle"
+              onClick={this.hide}
+            >
+              Скрыть
+            </button> */}
+
+        {visible && (
+          <div className="Dropdown__menu">Выпадающее меню</div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Dropdown;
