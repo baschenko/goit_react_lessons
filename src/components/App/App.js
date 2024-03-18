f68ade94930cb21b1bb5ad5688745a9dd50f85f1
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-03-19 01:15:45 +0200

    add lesson

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,15 @@
-export const App = () => {
-  return ;
-};
+import { GlobalStyle } from 'components/GlobalStyle';
+import { LoginForm } from 'components/LoginForm/LoginForm';
+import { ProductReviewForm } from 'components/ProductReviewForm/ProductReviewForm';
+import { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return <>
+      <GlobalStyle />
+      {/* <LoginForm/> */}
+      <ProductReviewForm/>
+
+      </>;
+  }
+}
